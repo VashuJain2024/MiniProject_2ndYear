e39840723fd101f6ecc6cafec71645056b70b209
Vashu Jain <mr.vashujain3020@gmail.com>
2024-11-16 11:15:38 +0530

    Change path

--- singleProduct.js
+++ singleProduct.js
@@ -1,4 +1,4 @@
-const images = ["https://vashujain2024.github.io/MiniProject_2ndYear/ImageSingleProductPage/image2.png", "https://vashujain2024.github.io/MiniProject_2ndYear/ImageSingleProductPage/image2.png", "https://vashujain2024.github.io/MiniProject_2ndYear/ImageSingleProductPage/image2.png", "https://vashujain2024.github.io/MiniProject_2ndYear/ImageSingleProductPage/image2.png", "https://vashujain2024.github.io/MiniProject_2ndYear/ImageSingleProductPage/image2.png"];
+const images = ["https://vashujain2024.github.io/MiniProject_2ndYear/ImageSingleProductPage/image1.png", "https://vashujain2024.github.io/MiniProject_2ndYear/ImageSingleProductPage/image2.png", "https://vashujain2024.github.io/MiniProject_2ndYear/ImageSingleProductPage/image3.png", "https://vashujain2024.github.io/MiniProject_2ndYear/ImageSingleProductPage/image4.png", "https://vashujain2024.github.io/MiniProject_2ndYear/ImageSingleProductPage/image5.png"];
 
 let swapImage = document.getElementById("swapImage");
 let mainImage = document.getElementById("mainImage");
