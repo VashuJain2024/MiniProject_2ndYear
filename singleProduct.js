000bfbd4e9ca2e4e0c6201814ce979fdd50babdd
Vashu Jain <mr.vashujain3020@gmail.com>
2024-11-16 11:27:59 +0530

    Add app file

--- singleProduct.js
+++ singleProduct.js
@@ -16,6 +16,17 @@ function swapimage() {
     }
 }
 
+function openRealifyApp() {
+    // Attempt to open the app using the custom URL scheme
+    window.location.href = "realify://open";
+
+    // Optional fallback for devices without the app
+    setTimeout(() => {
+        alert("Realify app is not installed on your device.");
+    }, 2000); // Wait for 2 seconds before showing the fallback
+}
+
+
 function startCamera() {
     // Check if the browser supports getUserMedia
     // const blocks = document.getElementById("blocks");
