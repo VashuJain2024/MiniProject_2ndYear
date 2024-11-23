f121091ee1caaba1a1a2f6938ce561b0e2ad02a1
Vashu Jain <mr.vashujain3020@gmail.com>
2024-11-23 10:33:10 +0530

    add

--- singleProduct.js
+++ singleProduct.js
@@ -1,4 +1,4 @@
-const images = ["https://vashujain2024.github.io/realify/ImageSingleProductPage/image1.jpg", "https://vashujain2024.github.io/realify/ImageSingleProductPage/image2.jpg", "https://vashujain2024.github.io/realify/ImageSingleProductPage/image3.png", "https://vashujain2024.github.io/realify/ImageSingleProductPage/image4.png", "https://vashujain2024.github.io/realify/ImageSingleProductPage/image5.png"];
+const images = ["https://vashujain2024.github.io/realify/ImageSingleProductPage/image1.png", "https://vashujain2024.github.io/realify/ImageSingleProductPage/image2.png", "https://vashujain2024.github.io/realify/ImageSingleProductPage/image3.png", "https://vashujain2024.github.io/realify/ImageSingleProductPage/image4.png", "https://vashujain2024.github.io/realify/ImageSingleProductPage/image5.png"];
 
 let swapImage = document.getElementById("swapImage");
 let mainImage = document.getElementById("mainImage");
